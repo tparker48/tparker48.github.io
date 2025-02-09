e514fb4bf03ce3d5fd644da164451d891c3872e8
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2025-02-09 14:46:51 -0800

    add natural notes only option

--- src/components/FretboardTrainer.js
+++ src/components/FretboardTrainer.js
@@ -4,20 +4,26 @@ import React, { useEffect } from 'react'
 import { useState } from 'react';
 
 const notes = ['E', 'F', 'F# Gf', 'G', 'G# Af', 'A', 'A# Bf', 'B', 'C', 'C# Df', 'D', 'D# Ef']
-
+const natural_only = ['E', 'F', 'G', 'A', 'B', 'C', 'D']
 
 const FretboardTrainer = () => {
   const [active_string, setActiveString] = useState(0);
   const [target_note, setTargetNote] = useState(0);
   const [feedback, setFeedback] = useState(false);
+  const [use_natural_only, setUseNaturalOnly] = useState(0);
 
-  useEffect(() => {
-    console.log(feedback);
-  }, [feedback]);
+  // useEffect(() => {
+  // // Code to run after every render or when dependencies change
+  // }, [target_note, use_natural_only]);
 
-  const get_random_note = () => {
-    let new_idx = (Math.floor(Math.random() * notes.length));
-    return (new_idx === target_note)  ? ((new_idx+1) % notes.length) : new_idx
+  const get_random_note = (use_nat_only) => {
+    let note_selection = use_nat_only ? natural_only : notes
+    let new_idx = (Math.floor(Math.random() * note_selection.length));
+    if (notes.indexOf(note_selection[new_idx]) == target_note) {
+      new_idx = (new_idx+1) % note_selection.length;
+    }
+    let rand_note = notes.indexOf(note_selection[new_idx])
+    return rand_note
   }
 
   const register_fret = (string, fret) => {
@@ -28,7 +34,7 @@ const FretboardTrainer = () => {
     let note = notes[(notes.indexOf(string_open_note)+fret)%(notes.length)];
     if (note === notes[target_note]) {
       setFeedback(true);
-      setTargetNote(get_random_note());
+      setTargetNote(get_random_note(use_natural_only));
     }
     else {
       setFeedback(false)
@@ -50,7 +56,6 @@ const FretboardTrainer = () => {
       }
     }
     
-
     let rgb = magnitude * 255;
     return `rgb(${rgb*0.7},${rgb*1.0},${rgb*0.9})`
   }
@@ -64,22 +69,16 @@ const FretboardTrainer = () => {
     return ""
   }
 
-  const handleStringSelect = (e) => {
-    console.log(e.target.value)
-    setActiveString(e.target.value);
-  }
-
   return (
     <Stack direction={"row"} bgcolor={"#111111"} spacing={1}>
     <Stack direction={"column"} spacing={10} padding={1} alignItems={"center"}>
-      <Typography variant={'h3'} width={50} height={50} margin={1} color={feedback? "green": "textSecondary"}>{notes[target_note]}</Typography>
+      <Typography variant={'h3'} width={50} height={50} margin={1} color={feedback? "green": "textSecondary"}>{ notes[target_note] }</Typography>
       <FormControl>
-        <FormLabel id="radio-string-selector-label" sx={{color:"#FFFFFF"}}>String</FormLabel>
+        <FormLabel id="radio-note-selector-label" sx={{color:"#FFFFFF"}}>String</FormLabel>
         <RadioGroup
-          aria-labeledby="radio-string-selector-label"
           defaultValue={0}
           name="radio-string-selector-group"
-          onChange={handleStringSelect}
+          onChange={(e)=>{setActiveString(e.target.value)}}
         >
           <FormControlLabel value={0} control={<Radio sx={{color:"#888888"}}/>} label="Any" />
           <FormControlLabel value={6} control={<Radio sx={{color:"#888888"}}/>} label="6" />
@@ -90,13 +89,24 @@ const FretboardTrainer = () => {
           <FormControlLabel value={1} control={<Radio sx={{color:"#888888"}}/>} label="1" />
         </RadioGroup>
       </FormControl>
+      <FormControl>
+        <FormLabel id="radio-note-selector-label" sx={{color:"#FFFFFF"}}>Notes</FormLabel>
+        <RadioGroup
+          defaultValue={0}
+          name="radio-note-selector-group"
+          onChange={(e)=>{setUseNaturalOnly(e.target.value); setTargetNote(get_random_note(e.target.value==1))}}
+        >
+          <FormControlLabel value={0} control={<Radio sx={{color:"#888888"}}/>} label="All" />
+          <FormControlLabel value={1} control={<Radio sx={{color:"#888888"}}/>} label="Natural" />
+        </RadioGroup>
+      </FormControl>
     </Stack>
     <Stack>
       {[0,1,2,3,4,5,6,7,8,9,10,11,12].map((fret)=>{
         return (
-          <div>
+          <div key = {fret}>
           <Stack
-            direction={'horizontal'} justifyContent={"center"}>
+            direction={"row"} justifyContent={"center"}>
             <Box alignContent={"center"} sx={{ borderBottom: 2,  borderColor: '#888888', bgcolor: get_fret_color(6, fret)}} width={"5.0vh"} height={"6.2vh"}  onClick={()=>{register_fret(6, fret)}}>{get_fret_text(6, fret)}</Box>
             <Box alignContent={"center"} sx={{ borderBottom: 2,  borderColor: '#888888', bgcolor: get_fret_color(5, fret)}} width={"5.0vh"} height={"6.2vh"}  onClick={()=>{register_fret(5, fret)}}>{get_fret_text(5, fret)}</Box>
             <Box alignContent={"center"} sx={{ borderBottom: 2,  borderColor: '#888888', bgcolor: get_fret_color(4, fret)}} width={"5.0vh"} height={"6.2vh"}  onClick={()=>{register_fret(4, fret)}}>{get_fret_text(4, fret)}</Box>
